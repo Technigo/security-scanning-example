e45c932ac17cc01307832523228bfa8f8591de14
Linnéa Oxenwaldt <linnea.oxenwaldt@omegapoint.se>
2026-01-15 10:14:41 +0100

    added code

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const reports = require('./routes/reports')
+const files = require('./routes/files')
+const search = require('./routes/search')
+const users = require('./routes/users')
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/users', users)
+
+app.get('/', (req, res) => {
+  res.send('security-scanning-example running')
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Listening on ${port}`)
+})
